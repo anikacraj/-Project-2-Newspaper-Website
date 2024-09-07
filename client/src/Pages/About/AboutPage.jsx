c1c215db40a5068e049f7e0b76bfe6b5df0ab19f
anikacraj <acdevilliersanik17@gmail.com>
2024-09-07 16:41:19 +0530

    Adding Components

--- client/src/Pages/About/AboutPage.jsx
+++ client/src/Pages/About/AboutPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/footer'
 
 function AboutPage() {
   return (
     <>
 
 <Header />
+<h1>This is about page </h1>
 
+<Footer />
     </>
   )
 }
